cdcabec74a68c791141e4f253c560ffd5abb5341
Michael Harris <122179098+Oshkosh1922@users.noreply.github.com>
2023-05-09 21:45:52 -0500

    Update hippie.js

--- js/hippie.js
+++ js/hippie.js
@@ -1,8 +1,12 @@
 const generateNumberBtn = document.getElementById("generate-number-btn");
 const randomNumberContainer = document.getElementById("random-number-container");
+const tweetDialog = document.getElementById("tweetDialog");
 
 generateNumberBtn.addEventListener("click", () => {
-  const randomNum = Math.floor(Math.random() * 99999999) + 1;
+  // Hide the Button of Fate
+  generateNumberBtn.style.display = "none";
+
+  const randomNum = Math.floor(Math.random() * 10000000) + 1;
   randomNumberContainer.innerHTML = '';
 
   randomNum.toString().split('').forEach((digit) => {
@@ -22,5 +26,25 @@ generateNumberBtn.addEventListener("click", () => {
     setTimeout(() => {
       digitBox.style.animation = "glow 2s ease-in-out infinite";
     }, 1000);
+  });
+
+  // Create and configure the Twitter share button
+  tweetDialog.innerHTML = `
+    <div style="background-color: darkblue; padding: 10px; border-radius: 5px; text-align: center;">
+      <p style="font-weight: bold; color: orange; margin: 0;">Please Share your number on Twitter to qualify</p>
+    </div>
+    <div style="margin-top: 10px;"></div>
+    <button id="shareOnTwitterBtn" class="btn btn-primary">Share on Twitter</button>
+  `;
+
+  // Center the "Share on Twitter" button
+  const shareOnTwitterBtn = document.getElementById("shareOnTwitterBtn");
+  shareOnTwitterBtn.style.display = "block";
+  shareOnTwitterBtn.style.margin = "0 auto";
+
+  shareOnTwitterBtn.addEventListener("click", () => {
+    const tweetText = encodeURIComponent(`My lucky number for the @TribeKapu Lottery: ${randomNum} #KapuTribeLottery`);
+    const tweetURL = `https://twitter.com/intent/tweet?text=${tweetText}`;
+    window.open(tweetURL, '_blank');
   });
 });
